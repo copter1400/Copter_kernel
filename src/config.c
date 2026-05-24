ff510327c5c32eb15af1ea6feb3bc9512172d9cc
Copter <phontanc@gmail.com>
2026-05-24 13:14:30 +0700

    make kshell now use heap

--- src/config.c
+++ src/config.c
@@ -3,7 +3,11 @@
 kernel_config_t kernel_config;
 
 void config_load() {
-    kernel_config.vga_width = 80;
-    kernel_config.vga_height = 25;
-    kernel_config.vga_color = 0x0F;
+    kernel_config.vga_width         = 80;
+    kernel_config.vga_height        = 25;
+    kernel_config.vga_color         = 0x0F;
+    kernel_config.vga_color_fail    = 0x04;
+    kernel_config.vga_color_warning = 0x0E;
+    kernel_config.vga_color_ok      = 0x02;
+    kernel_config.kshell_buffer     = 256;
 }
--- src/kshell.c
+++ src/kshell.c
@@ -6,9 +6,14 @@
 #include "timer.h"
 #include "panic.h"
 #include "multiboot.h"
+#include "heap.h"
+#include "kernel.h"
+#include "config.h"
 
 void shell_task() {
-    char buffer[256];
+    config_load();
+
+    char* buffer = kmalloc(kernel_config.kshell_buffer);
     uint8_t pos = 0;
 
     print(">");
@@ -32,7 +37,7 @@ void shell_task() {
         } else if (c == '\b' && pos > 0) {
             terminal_backspace();
             pos--;
-        } else if (pos < 255) {
+        } else if (pos < kernel_config.kshell_buffer - 1) {
             buffer[pos] = c;
             char str[2] = {c,0};
             print(str);
@@ -42,51 +47,78 @@ void shell_task() {
 }
 
 void shell_exec(char* buffer) {
-
-    char* arg;
+    int argc = 0;
     int i = 0;
+    int in_word = 0;
 
-    while (buffer[i]) {
+    // argc part
+    while (buffer[i] != '\0') {
+        if (buffer[i] != ' ' && !in_word) {
+            in_word = 1;
+            argc++;
+        }
+
         if (buffer[i] == ' ') {
-            buffer[i] = 0;
-            arg = &buffer[i + 1];
-            
-            break;
+            buffer[i] = '\0';
+            in_word = 0;
         }
 
         i++;
     }
 
-    if (strcmp(buffer, "") == 0) {
+    // argv part
+    char **argv = kmalloc(sizeof(char*) * (argc + 1));
+    int a = 0;
+    for (int i = 0; i < kernel_config.kshell_buffer; i++) {
+
+        if (buffer[i] != '\0' &&
+            (i == 0 || buffer[i - 1] == '\0')) {
+
+            argv[a++] = &buffer[i];
+        }
+    }
+    argv[a] = 0;
+
+    if (strcmp(argv[0], "") == 0 || argc == 0) {
         
     }
 
     // test command
-    else if (strcmp(buffer, "dvz") == 0) {
+    else if (strcmp(argv[0], "dvz") == 0) {
         asm volatile("int $0");
     }
 
     // real command
-    else if   (strcmp(buffer, "help") == 0) {
+    else if   (strcmp(argv[0], "help") == 0) {
         print("Commands: help hello tick clear echo dump-ram dvz panic\n");
-    } else if (strcmp(buffer, "hello") == 0) {
+    } else if (strcmp(argv[0], "hello") == 0) {
         print("Hello, World!\n");
-    } else if (strcmp(buffer, "tick") == 0) {
+    } else if (strcmp(argv[0], "tick") == 0) {
         print_int(timer_ticks);
         print("\n");
-    } else if (strcmp(buffer, "clear") == 0) {
+    } else if (strcmp(argv[0], "clear") == 0) {
         clear_screen();
-    } else if (strcmp(buffer, "echo") == 0) {
-        print(arg);
+    } else if (strcmp(argv[0], "echo") == 0) {
+        for (int i = 1; i < argc; i++) {
+            print(argv[i]);
+            print(" ");
+        }
         print("\n");
-    } else if (strcmp(buffer, "dump-ram") == 0) {
+    } else if (strcmp(argv[0], "dump-ram") == 0) {
         kernel_get_memory_info();
-        kernel_get_memory_region(0x100000 + atoi(arg), 10);
-    } else if (strcmp(buffer, "panic") == 0) {
-        panic(arg);
+        kernel_get_memory_region(0x100000 + atoi(argv[0]), 10);
+    } else if (strcmp(argv[0], "panic") == 0) {
+        panic(argv[1]);
     }
 
     else {
-        print("Unknown command\n");
+        print("Unknown command [");
+        print(argv[0]);
+        print("]");
+        print("\n");
     }
+
+    // clear argv argc
+    argv[0] = NULL;
+    argc    = 0;
 }
